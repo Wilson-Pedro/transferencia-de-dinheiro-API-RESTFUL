ecb0b0a21f012dafe923279e9d6e5018b6ed7d56
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-06-09 07:44:30 -0300

    Método POST

--- src/main/java/com/wamkti/wamk/controllers/ClienteController.java
+++ src/main/java/com/wamkti/wamk/controllers/ClienteController.java
@@ -1,10 +1,19 @@
 package com.wamkti.wamk.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wamkti.wamk.entities.Cliente;
+import com.wamkti.wamk.entities.dtos.ClienteRecodDTO;
 import com.wamkti.wamk.services.ClienteService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/clientes")
@@ -13,5 +22,10 @@ public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 	
-	
+	@PostMapping
+	public ResponseEntity<Cliente> salvarCliente(@Valid @RequestBody ClienteRecodDTO clienteRecodDTO) {
+		var cliente = new Cliente();
+		BeanUtils.copyProperties(clienteRecodDTO, cliente);
+		return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.save(cliente));
+	}
 }
--- src/main/java/com/wamkti/wamk/services/ClienteService.java
+++ src/main/java/com/wamkti/wamk/services/ClienteService.java
@@ -3,6 +3,7 @@ package com.wamkti.wamk.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wamkti.wamk.entities.Cliente;
 import com.wamkti.wamk.repositories.ClienteRepository;
 
 @Service
@@ -10,4 +11,8 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
+
+	public Cliente save(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
 }
