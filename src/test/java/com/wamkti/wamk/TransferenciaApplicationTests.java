96f7f0d5bc0722fbb37c3489051894683642fadf
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-06-09 07:19:39 -0300

    first commit

---
+++ src/test/java/com/wamkti/wamk/TransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wamkti.wamk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
